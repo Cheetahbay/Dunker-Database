f812c6640617ea021409edb6caa72cfe6f5c15a3
Chidebe Ndibe <chideben@gmail.com>
2021-10-11 01:29:34 -0400

    Added random dunker button

--- app.py
+++ app.py
@@ -4,9 +4,10 @@ from flask import Flask, render_template, url_for, redirect
 from flask.helpers import flash
 from flask.wrappers import Response
 from forms import DunkerForm
-from pymongo import MongoClient
+from pymongo import MongoClient, collection
 import json
 from dotenv import dotenv_values
+import random
 
 config = dotenv_values(".env")
 app = Flask(__name__)
@@ -44,6 +45,17 @@ def dunker_view(dunker_name):
     else:
         flash(f"Dunker: {dunker_name} Not Found", "warning")
         return redirect(url_for("index"))
+
+@app.route("/random")
+def random_dunker():
+    db = client["DunkerDB"]
+    collection = db["dunkers"]
+    dunker_cursor = collection.find({"Name":{"$exists": True}})
+    names = [document["Name"] for document in dunker_cursor]
+    random_num = random.randint(0, len(names))
+    random_dunker = collection.find_one({"Name": names[random_num]})
+    # random_dunker = collection.aggregate([{'$sample': {'size': 1 }}], batchSize=1)
+    return redirect(url_for("dunker_view", dunker_name=random_dunker["Name"]))
 
 if __name__ == "__main__":
     app.run(debug=True)
